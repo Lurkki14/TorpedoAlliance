db2af860b032e811f3d12144bc4af60119622eb0
jussi <jussi.kuokkanen@protonmail.com>
2020-01-25 16:44:45 +0200

    make the car suck less

--- VehicleBody.gd
+++ VehicleBody.gd
@@ -1,18 +1,35 @@
 extends VehicleBody
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func rotation(event : InputEventKey):
+	match char(event.scancode):
+		"A": return 0.8
+		"D": return -0.8
+	return 0
+
+func force(event : InputEventKey):
+	match char(event.scancode):
+		"W": return 400
+		"S": return -400
+	return 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_max_contacts_reported(3)
+	set_engine_force(0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
+func _process(delta):
+	print("engine force: ", get_engine_force(), " steering: ", get_steering())
 
 func _input(event):
-	if event is InputEventKey and event.is_pressed() and char(event.scancode) == "W":
-		engine_force = 300
-	if event is InputEventKey and event.is_pressed() and char(event.scancode) == "A":
-		steering = -0.8
+	if event is InputEventKey and event.is_pressed():
+		if rotation(event) != NAN:
+			print("setting steering to ", rotation(event))
+			set_steering(rotation(event))
+		
+		if force(event) != NAN:
+			print("setting force to ", force(event))
+			set_engine_force(force(event))
+			
+	if event is InputEventKey and not event.is_pressed():
+		set_steering(0)
+		set_engine_force(0)
