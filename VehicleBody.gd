ecb235cc3a94673f5f474007cc007cd07717a177
jussi <jussi.kuokkanen@protonmail.com>
2020-01-29 18:08:10 +0200

    add pitch and yaw to car

--- VehicleBody.gd
+++ VehicleBody.gd
@@ -10,9 +10,30 @@ const STEER_LIMIT = 0.7
 var steer_angle = 0
 var steer_target = 0
 
+var pitch_force = 400
+
 var count = 0
 
 export var engine_force_value = 400
+
+func countertorque() -> Vector3:
+	var vel_vec = get_angular_velocity()
+	var magnitude = sqrt(pow(2, vel_vec.x) + pow(2, vel_vec.y) + pow(2, vel_vec.z))
+	if magnitude < 0.1:
+		return Vector3(0, 0, 0)
+	var ang_vel_sqr = vel_vec.x*vel_vec.x + vel_vec.y*vel_vec.y + vel_vec.z*vel_vec.z
+	var ang_vel = sqrt(ang_vel_sqr)
+	if ang_vel < 0.1:
+		return Vector3(0, 0, 0)
+	var ct_magnitude =  (-pitch_force / 32) * ang_vel_sqr
+	var ct = Vector3(ct_magnitude*vel_vec.x/ang_vel, ct_magnitude*vel_vec.y/ang_vel, ct_magnitude*vel_vec.z/ang_vel)
+	return ct
+
+func torque(basis : Vector3, force : int) -> Vector3:
+	var fx = basis.x * force
+	var fy = basis.y * force
+	var fz = basis.z * force
+	return Vector3(fx, fy, fz)
 
 func get_contact():
 	for element in wheel_contact:
@@ -22,6 +43,24 @@ func get_contact():
 		
 func _physics_process(delta):
 	var fwd_mps = transform.basis.xform_inv(linear_velocity).x
+	
+	if Input.is_action_just_pressed("car_jump") and car_jump > 0:
+		apply_central_impulse(Vector3(0, 600, 0))
+		car_jump -= 1
+	
+	var ct = countertorque()
+	
+	if Input.is_action_pressed("car_pitch_up") and car_jump < 2:
+		apply_torque_impulse(ct - torque(get_global_transform().basis.x, pitch_force))
+		
+	if Input.is_action_pressed("car_pitch_down") and car_jump < 2:
+		apply_torque_impulse(ct + torque(get_global_transform().basis.x, pitch_force))
+		
+	if Input.is_action_pressed("car_yaw_left") and car_jump < 2:
+		apply_torque_impulse(ct + torque(get_global_transform().basis.y, pitch_force))
+		
+	if Input.is_action_pressed("car_yaw_right") and car_jump < 2:
+		apply_torque_impulse(ct - torque(get_global_transform().basis.y, pitch_force))
 	
 	if Input.is_action_pressed("car_steer_left"):
 		steer_target = STEER_LIMIT
