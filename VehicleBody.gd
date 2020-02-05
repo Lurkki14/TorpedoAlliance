f9df58bba9c954f100342405d2c1bcf8aa85ebcb
jussi <jussi.kuokkanen@protonmail.com>
2020-02-05 15:45:10 +0200

    add forward dodge to car

--- VehicleBody.gd
+++ VehicleBody.gd
@@ -24,11 +24,21 @@ var ct_constant = pitch_force/64
 var refspeed = 10
 var max_downforce = 50
 
+var dodge_active: bool
+var dodge_basis: Vector3
+
+var dodge_prev_vel: float
+var dodge_time: float
+var dodge_start_vec: Vector3
+var dodge_delta_vec: Vector3
+var dodge_prev_vec: Vector3
+
 var count = 0
 
 export var engine_force_value = 400
 
 func _ready():
+	dodge_active = false
 	set_contact_monitor(true)
 	set_max_contacts_reported(1)
 	for node in get_children():
@@ -71,8 +81,19 @@ func get_contact() -> bool:
 			return false
 	return true
 
+func _integrate_forces(state : PhysicsDirectBodyState):
+	if dodge_active:
+		dodge_time += state.step
+		print(dodge_time)
+		# Detect 360 rotation
+		if dodge_time * 10 > 2 * PI:
+			dodge_active = false
+		else:
+			state.set_angular_velocity(dodge_basis * 10)
+
 func _physics_process(delta):
-	print(car_jump)
+	#print(car_jump)
+	#print(get_rotation())
 	if contact_ignore_time > 0:
 		contact_ignore_time -= delta
 	
@@ -84,20 +105,30 @@ func _physics_process(delta):
 	var fwd_mps = transform.basis.xform_inv(linear_velocity).x
 	
 	if Input.is_action_pressed("car_boost"):
-		print(boost(get_global_transform().basis.z))
+		#print(boost(get_global_transform().basis.z))
 		apply_central_impulse(boost(get_global_transform().basis.z))
 	
 	if Input.is_action_just_pressed("car_jump") and car_jump > 0:
-		if contact_ignore_time <= 0:
+		
+		# Dodge jump
+		if Input.is_action_pressed("car_accelerate") and car_jump == 1:
+			print("dodge start")
+			dodge_time = 0
+			dodge_basis = get_global_transform().basis.x
+			apply_central_impulse(get_global_transform().basis.z * pitch_force * 2)
+			dodge_active = true
+		else:
+			if contact_ignore_time <= 0:
+				contact_ignore_time = CONTACT_IGNORE_MAX_TIME
 			contact_ignore_time = CONTACT_IGNORE_MAX_TIME
-		contact_ignore_time = CONTACT_IGNORE_MAX_TIME
-		var y_basis = get_global_transform().basis.y
-		var force_vector = JUMP_FORCE * y_basis
-		apply_central_impulse(force_vector)
+			var y_basis = get_global_transform().basis.y
+			var force_vector = JUMP_FORCE * y_basis
+			apply_central_impulse(force_vector)
 		car_jump -= 1
 	
-	var ct = countertorque()
-	apply_torque_impulse(ct)
+	if not dodge_active:
+		var ct = countertorque()
+		apply_torque_impulse(ct)
 	
 	if Input.is_action_pressed("car_pitch_up") and not wheel_contact() and not body_contact():
 		apply_torque_impulse(-torque(get_global_transform().basis.x, pitch_force))
