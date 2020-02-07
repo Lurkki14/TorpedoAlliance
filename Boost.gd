5ccc8cb68dc3768f88653c6f8a4cc4c06ba94363
jussi <jussi.kuokkanen@protonmail.com>
2020-02-07 16:22:25 +0200

    add boost script

---
+++ Boost.gd
@@ -0,0 +1,16 @@
+extends Particles
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_emitting(false)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("car_boost"):
+		set_emitting(true)
+	else:
+		set_emitting(false)
