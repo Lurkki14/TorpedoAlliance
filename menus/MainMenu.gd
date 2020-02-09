9c20993d27e22b1994ed64f205a346b922dc2152
Ott Joon <ott.joon@tutanota.com>
2020-02-09 20:14:54 +0200

    Mone options pop-up

--- menus/MainMenu.gd
+++ menus/MainMenu.gd
@@ -1,8 +1,17 @@
 extends Control
 
-func _change_scene(scene):
+func _load_scene(scene: String):
+	var loaded_scene = load(scene).instance()
+	add_child(loaded_scene)
+
+func _change_scene(scene: String):
 	queue_free()
 	get_tree().change_scene(scene)
 
 func _on_Play_pressed(button):
 	_change_scene("res://Field.tscn")
+
+func _on_Options_pressed():
+	var scene = preload("res://menus/OptionsMenu.tscn").instance()
+	add_child(scene)
+	scene.get_node("PopupMenu").show()
